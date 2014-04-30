70c8ceee4f35998504190160ae5e5120f99ddc4a
David Beck <david@rotundasoftware.com>
2014-04-30 14:50:43 -0700

    add initialize function

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 A simple backbone inspired, coffee compatible class object for node.js and the browser.
 
+Commonjs only - use [browserify](https://github.com/substack/node-browserify) or [cartero](https://github.com/rotundasoftware/cartero)) for the browse
+
 ```javascript
 Class = require( 'class-con-leche' );
 
 Walrus = Class.extend( {
+	initialize : function() {
+		// called when class instance is created
+		console.log( 'I am a baby walrus' );
+	},
+
 	helloWorld : function() {
 		console.log( 'I am a walrus' );
 	}
--- class.js
+++ class.js
@@ -5,7 +5,18 @@
  * http://github.com/rotundasoftware/backbone.handle
  */
 
-(function() {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        // Node, browserify, cartero
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.ClassConLeche = factory();
+  }
+}(this, function () {
     var fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
    
     // The base Class implementation (does nothing)
@@ -14,34 +25,38 @@
     // Create a new Class that inherits from this class
     Class.extend = function(prop) {
       var _super = this.prototype;
-     
+
       // Instantiate a base class (but only create the instance,
       // don't run the init constructor)
       initializing = true;
       var prototype = new this();
       initializing = false;
-     
+
       // Copy the properties over onto the new prototype
       for (var name in prop)
         prototype[name] = prop[name];
-     
+
       // The dummy class constructor
-      function Class() {}
-     
+      function Class() {
+        // All construction is actually done in the initialize method
+        if ( !initializing && this.initialize )
+          this.initialize.apply(this, arguments);
+      }
+
       // Populate our constructed prototype object
       Class.prototype = prototype;
-     
+
       // Enforce the constructor to be what we expect
       Class.prototype.constructor = Class;
-   
+
       // And make this class extendable
       Class.extend = arguments.callee;
-     
+
       // for coffeescript compatibility
       Class.__super__ = _super;
 
       return Class;
     };
 
-    module.exports = Class;
-})();
+    return Class;
+}));
