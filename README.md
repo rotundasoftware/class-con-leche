41c62b9b8781c2771fd1d76cd7e569d2b4dcf994
David Beck <david@rotundasoftware.com>
2014-04-30 14:27:35 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+A simple backbone inspired, coffee compatible class object for node.js and the browser.
+
+Class = require( 'class-con-leche' );
+
+Walrus = Class.extend( {
+	helloWorld : function() {
+		console.log( 'I am a walrus' );
+	}
+} );
+
+AngryWalrus = Class.extend( {
+	helloWorld : function() {
+		Walrus.prototype.myFunction(); // call super.. no syntactical sugar here.
+
+		console.log( 'Get lost!' );
+	}
+} );
---
+++ class.js
@@ -0,0 +1,47 @@
+/*
+ * class-con-leche, v0.0.0
+ * Copyright (c)2014 Rotunda Software, LLC.
+ * Distributed under MIT license
+ * http://github.com/rotundasoftware/backbone.handle
+ */
+
+(function() {
+    var fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
+   
+    // The base Class implementation (does nothing)
+    Class = function(){};
+   
+    // Create a new Class that inherits from this class
+    Class.extend = function(prop) {
+      var _super = this.prototype;
+     
+      // Instantiate a base class (but only create the instance,
+      // don't run the init constructor)
+      initializing = true;
+      var prototype = new this();
+      initializing = false;
+     
+      // Copy the properties over onto the new prototype
+      for (var name in prop)
+        prototype[name] = prop[name];
+     
+      // The dummy class constructor
+      function Class() {}
+     
+      // Populate our constructed prototype object
+      Class.prototype = prototype;
+     
+      // Enforce the constructor to be what we expect
+      Class.prototype.constructor = Class;
+   
+      // And make this class extendable
+      Class.extend = arguments.callee;
+     
+      // for coffeescript compatibility
+      Class.__super__ = _super;
+
+      return Class;
+    };
+
+    module.exports = Class;
+})();
